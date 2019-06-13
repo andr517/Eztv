5560a6048599e22ffb6f60a601f80339d2c6316d
Johan Westin <johan.b.westin@gmail.com>
2019-06-13 16:08:14 +0200

    .

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('userName');
+            $table->string('userName')->unique();
             $table->string('email')->unique();
             $table->integer('emailAnswer');
             $table->string('picture')->nullable();
